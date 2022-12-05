dfb7120e2c5e0cf35ae27daf468cea7e29650b80
Martina <martinasorialanzi@>
2022-12-05 19:00:48 -0300

    Creacion carpetas components,api,assets

---
+++ src/components/routes/Rutas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+
+
+const Rutas = () => {
+  return (
+    <Routes>
+       
+    </Routes>
+  )
+}
+
+export default Rutas

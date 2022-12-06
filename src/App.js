f5d23a4d11d2222d6829d8cf7d3ec1170fcb5a2c
Martina <martinasorialanzi@>
2022-12-06 11:23:36 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Rutas from './components/routes/Rutas';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
   <>
+  <Rutas/>
   </>
   );
 }
--- src/components/routes/Rutas.jsx
+++ src/components/routes/Rutas.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Routes, Route} from "react-router-dom"
+import Ecommerce from '../pages/Ecommerce'
 
 
 const Rutas = () => {
   return (
     <Routes>
-       
+       <Route path='/ecommerce' element={Ecommerce}/>
     </Routes>
   )
 }

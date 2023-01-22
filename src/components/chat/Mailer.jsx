e232691da4b3cede35d97aa6734127c83c676ba9
LucasLon22 <crlucaslondero@gmail.com>
2023-01-22 19:18:17 -0300

    chat

--- src/components/chat/Mailer.jsx
+++ src/components/chat/Mailer.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import { useRef } from 'react';
-
+import { useEffect } from 'react';
 import { Container, } from 'react-bootstrap';
-
 import emailjs from '@emailjs/browser';
+import './Mailer.css'
 
 
 const Mailer = () => {
-    const form = useRef();
+    
+    const inputform = useRef(null); 
+
+    useEffect(() => {
+        inputform.current.focus()
+      
+      })
+    
+    
 
     const sendEmail = (e) => {
         e.preventDefault();
         emailjs.sendForm('service_o1bi7xw', 'template_5llduc9', e.target, 'ZeW1Ssurf-xesG-Ui').then(res => console.log(res))
-        .catch(error=>console.log(error))
+        e.target.reset()
+            .catch(error => console.log(error))
+        
     }
+    
 
 
   return (
       <Container>
-          <form ref={form} onSubmit={sendEmail} className="text-center">
-              <label>Nombre</label>
-              <input type="text" name="user_name" />
+          <form  onSubmit={sendEmail} className="text-center">
+              <label >Nombre</label>
+              <input type="text" name="user_name" ref={inputform} />
               <hr />
               <label>Email</label>
-              <input type="email" name="user_email" />
+              <input type="email" name="user_email"  />
               <hr />
               <label>Mensaje</label>
               <textarea name="user_message" />
               <hr />
-              <button variant="outline-success">Success</button>
+              <button className='mailer-color-boton' >Enviar</button>
           </form>
       </Container>
   )

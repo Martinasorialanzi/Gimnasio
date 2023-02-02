978259b489852d2f4fb8e6623c31ae3a12f83b9c
martinasorialanzi <martinasorialanzi@gmail.com>
2023-02-02 15:11:29 -0300

    correcion ruta get prodcts

--- src/api/GetProducts.jsx
+++ src/api/GetProducts.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://gimansio-backend-4phx-git-main-martinasorialanzi.vercel.app/";
+const baseUrl = "https://gimansio-backend-4phx-git-main-martinasorialanzi.vercel.app";
 
 export const GetProducts = async () => {
   try {

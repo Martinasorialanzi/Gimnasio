c206fb647f97a81632ab97bdab874ebff07c1677
LucasLon22 <crlucaslondero@gmail.com>
2023-02-02 16:42:42 -0300

    marti

--- src/api/GetProducts.jsx
+++ src/api/GetProducts.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8080/v1";
+const baseUrl = "https://gimansio-backend.vercel.app";
 
 export const GetProducts = async () => {
   try {

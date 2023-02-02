500e4adafb53302a3734b82e4c1c346d990590de
martinasorialanzi <martinasorialanzi@gmail.com>
2023-02-02 15:17:34 -0300

    ruta localhost/8080

--- src/api/GetProducts.jsx
+++ src/api/GetProducts.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://gimansio-backend-4phx-git-main-martinasorialanzi.vercel.app";
+const baseUrl = "http://localhost:8080/v1";
 
 export const GetProducts = async () => {
   try {
